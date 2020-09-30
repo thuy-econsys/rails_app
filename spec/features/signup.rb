f871cb2e056666863306f031c7617767f298c315
Thuy Ngo <thuy.ngo@econsys.com>
2020-09-30 13:34:43 -0400

    Correct signup spec issues; pass all tests

--- spec/features/signup.rb
+++ spec/features/signup.rb
@@ -1,42 +1,45 @@
 require 'rails_helper'
-  # TODO troubleshoot invalid form inputs as user param outputs let symbols? 
 
 feature 'Sign up' do
   let(:test_user) { attributes_for(:user) }
-  let(:no_email) { attributes_for(:user, email: "")}
-  let(:password_not_a_match) { attributes_for(:user, password_confirmation: "wrongpassword") }
 
   context 'when sign up is successful' do
     scenario 'redirects to home' do
-      sign_up(:test_user)
+      sign_up(test_user)
       expect(current_path).to eq('/')
     end
     scenario 'has correct content' do
-      sign_up(:test_user)
+      sign_up(test_user)
       expect(page).to have_content('Welcome! You have signed up successfully.')
     end
   end
 
   context 'when email is blank' do
-    xscenario 'redirects to users path' do
+    scenario 'redirects to users path sign up page' do
       # byebug
-      sign_up(:no_email)
+      test_user[:email] =""
+      sign_up(test_user)
       expect(current_path).to eq('/users')
+      expect(page).to have_content('Sign up')
     end
-    xscenario 'returns error message' do
-      sign_up(:no_email)
+    scenario 'returns error message' do
+      test_user[:email] =""
+      sign_up(test_user)
       expect(page).to have_content('Email can\'t be blank')
     end
   end
 
   context 'when password confirmation doesn\'t match password' do
-    xscenario 'redirects to users path' do
+    scenario 'redirects to users path sign up page' do
       # byebug
-      sign_up(:password_not_a_match)
+      test_user[:password_confirmation] = "wrong-password"
+      sign_up(test_user)
       expect(current_path).to eq('/users')
+      expect(page).to have_content('Sign up')
     end
-    xscenario 'returns error message' do
-      sign_up(:password_not_a_match)
+    scenario 'returns error message' do
+      test_user[:password_confirmation] = "wrong-password"
+      sign_up(test_user)
       expect(page).to have_content('Password confirmation doesn\'t match Password')
     end
   end
--- spec/helpers/helpers.rb
+++ spec/helpers/helpers.rb
@@ -3,12 +3,9 @@ module TestHelper
     visit new_user_registration_path
     expect(current_path).to eq('/users/sign_up')
     expect(page).to have_content('Sign up')
-    fill_in(:user_email, :with => test_user[:email])
-    fill_in(:user_password, :with => test_user[:password])
-    fill_in(:user_password_confirmation, :with => test_user[:password_confirmation])
-    # fill_in(:user_email, :with => password_not_a_match[:email])
-    # fill_in(:user_password, :with => password_not_a_match[:password])
-    # fill_in(:user_password_confirmation, :with => password_not_a_match[:password_confirmation])
+    fill_in(:user_email, :with => user[:email])
+    fill_in(:user_password, :with => user[:password])
+    fill_in(:user_password_confirmation, :with => user[:password_confirmation])
     click_button('Sign up')
   end
 
