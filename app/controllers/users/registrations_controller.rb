a9562aa7656eb49d9d9e0a1f5a83b4a39e0aad39
Thuy Ngo <thuy.ngo@econsys.com>
2020-09-30 22:25:42 -0400

    Override Devise::RegistrationsController#create and sanitize params

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,65 @@
+# frozen_string_literal: true
+
+class Users::RegistrationsController < Devise::RegistrationsController
+  before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_account_update_params, only: [:update]
+
+  # GET /resource/sign_up
+  # def new
+  #   super
+  # end
+
+
+  # POST /resource
+  def create
+      super
+      UserMailer.admin_approval_email(@user).deliver_now
+  end
+
+  # GET /resource/edit
+  # def edit
+  #   super
+  # end
+
+  # PUT /resource
+  # def update
+  #   super
+  # end
+
+  # DELETE /resource
+  # def destroy
+  #   super
+  # end
+
+  # GET /resource/cancel
+  # Forces the session data which is usually expired after sign
+  # in to be expired now. This is useful if the user wants to
+  # cancel oauth signing in/up in the middle of the process,
+  # removing all OAuth session data.
+  # def cancel
+  #   super
+  # end
+
+  protected
+  # Devise permits by default email, password, and password_confirmation attributes
+    # So it's only necessary to append extra attributes to the sanitizer.
+  # NEVER whitelist 'approved' attribute as this will allow user to self approve
+
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:notes])
+  end
+
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: [:notes])
+  end
+
+  # The path used after sign up.
+  # def after_sign_up_path_for(resource)
+  #   super(resource)
+  # end
+
+  # The path used after sign up for inactive accounts.
+  # def after_inactive_sign_up_path_for(resource)
+  #   super(resource)
+  # end
+end
