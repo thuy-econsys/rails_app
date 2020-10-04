130ec31a629cac2a0b266d9f7c35e2ba92a85561
Thuy Ngo <thuy.ngo@econsys.com>
2020-10-04 03:11:22 -0400

    Add approved_user factorybot trait for rspec tests

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,5 +3,11 @@ FactoryBot.define do
     email { Faker::Internet.unique.email }
     password { "P@ssw0rd" }
     password_confirmation { "#{password}" }
+
+    trait :approved_user do
+      approved { true }
+    end
+
+    factory :approved_user, traits: [:approved_user]
   end
 end
