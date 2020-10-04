b510469e391c149f586312032485f4fcc8a09a69
Thuy Ngo <thuy.ngo@econsys.com>
2020-10-04 19:32:52 -0400

    build() does not work in MailerPreview; add correct url preview path

--- spec/mailers/previews/admin_mailer_preview.rb
+++ spec/mailers/previews/admin_mailer_preview.rb
@@ -1,6 +1,6 @@
-# http://localhost:3000/rails/mailers/admin_mailer/mailer_preview
+# http://localhost:3000/rails/mailers/admin_mailer/admin_mailer_preview
 class AdminMailerPreview < ActionMailer::Preview
   def admin_mailer_preview
-    AdminMailer.with(user: User.first).admin_approval_email
+    AdminMailer.with( user: User.first ).admin_approval_email
   end
 end
