45ea8c49213975a939b4dee42aec26e26cf75ed3
Thuy Ngo <thuy.ngo@econsys.com>
2020-10-04 03:16:12 -0400

    Remove incorrect Devise controller overriding syntax

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'users/registrations' }
-  # devise_for :users, :controllers => { registrations: 'registrations' }
 
   root to: "pages#home"
 end
