4fee18e517c3e9bd29bb3cbc4ab780baf325cf21
Thuy Ngo <thuy.ngo@econsys.com>
2020-10-04 19:49:46 -0400

    Clarify mailer header

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -1,10 +1,11 @@
 class AdminMailer < ApplicationMailer 
   default from: 'admin@thuy.life'
-  default to: 'admin@thuy.life'
-
+  
   def admin_approval_email
     mail(
-      subject: 'User awaiting Administrator Approval')
+      to: params[:user].email,
+      cc: 'admin@thuy.life',
+      subject: 'Thank you for signing up')
   end
 end
 
