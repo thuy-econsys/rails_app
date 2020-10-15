5588114be846daf33d41122ea8702c7e178863de
Thuy Ngo <thuy.ngo@econsys.com>
2020-10-15 19:28:17 -0400

    Move devise_parameter_sanitizer to application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  
+  protected
+  
+  #FIXME alternative solution for 'approved' attribute as this might allow user to self approve
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: [:notes])
+    devise_parameter_sanitizer.permit(:accept_invitation, keys: [:notes])
+  end
 end
--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
-  before_action :configure_sign_up_params, only: [:create]
-  before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
   # def new
@@ -38,19 +36,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # def cancel
   #   super
   # end
-
-  protected
-  # Devise permits by default email, password, and password_confirmation attributes
-    # So it's only necessary to append extra attributes to the sanitizer.
-  # NEVER whitelist 'approved' attribute as this will allow user to self approve
-
-  def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:notes])
-  end
-
-  def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: [:notes])
-  end
 
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
