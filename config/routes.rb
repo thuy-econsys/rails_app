c05e923104ac7ebaa9ad4c62aac7facf9d4bbb65
Thuy Ngo <thuy.ngo@econsys.com>
2020-10-21 19:59:24 -0400

    Add dashboard views of Users index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'users/registrations' }
 
+  get '/users', to: 'pages#index', as: :dashboard
+
   root to: "pages#home"
 end
