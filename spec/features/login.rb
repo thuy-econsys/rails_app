b2c1bdcb3e8b4b4a8ba816e60912f0cfa3f2cd23
Thuy Ngo <thuy.ngo@econsys.com>
2020-11-04 19:44:44 -0500

    Update specs for clearer expectations

--- spec/features/login.rb
+++ spec/features/login.rb
@@ -8,39 +8,41 @@ feature 'User login' do
       log_in(user_info)
       expect(current_path).to eq("/")
     end
-    scenario "returns successful sign-in message" do
+    scenario "has successful sign-in notice" do
       log_in(user_info)
-      expect(page).to have_content("Signed in successfully.")
+      expect(page).to have_css('p.notice', :text => 'Signed in successfully.')
     end
     scenario "has correct content" do
       log_in(user_info)
-      expect(page).to have_content("Home Page")
+      expect(page).to have_css('h1', :text => 'Home Page' )
     end
   end
 
   context "when email is incorrect" do
-    scenario "redirects to sign-in page" do
+    scenario "stays on sign-in page" do
       user_info.email = "wrong_email@gmail.com"
-      log_in(user_info) 
-      expect(current_path).to eq("/users/sign_in")  
+      log_in(user_info)
+      expect(current_path).to eq("/users/sign_in")
+      expect(page).to have_css('h1', :text => 'Log in')
     end
-    scenario "returns error message" do
+    scenario "returns error alert" do
       user_info.email = "wrong_email@gmail.com"
       log_in(user_info) 
-      expect(page).to have_content("Invalid Email or password.")
+      expect(page).to have_css('p.alert', :text => 'Invalid Email or password.')
     end
   end
 
   context "when password is incorrect" do
-    scenario 'redirects to sign-in page' do
+    scenario 'stays on sign-in page' do
       user_info.password = "wrongpassword"
       log_in(user_info)
       expect(current_path).to eq("/users/sign_in")
+      expect(page).to have_css('h1', :text => 'Log in')
     end
-    scenario "returns error message" do
+    scenario "returns error alert" do
       user_info.password = "wrongpassword"
       log_in(user_info)
-      expect(page).to have_content("Invalid Email or password.")
+      expect(page).to have_css('p.alert', :text => 'Invalid Email or password.')
     end
   end
 end
