fdff35a39d5a91ce9b685e0c72be0b5d8d6c0ebe
Thuy Ngo <thuy.ngo@econsys.com>
2020-11-06 11:32:48 -0500

    Update validations to validates_format_of

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,25 +5,16 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :trackable
 
   validates :phone, presence: true
-  validate :email_input, :password_input, :phone_input
-
-  def email_input
-    return if email =~ /\A(([a-z0-9][\w\.\-]{0,62}[a-z0-9])@(([a-z0-9][a-z0-9\-]{0,62}[a-z0-9]\.){1,8})([a-z]{2,63}))\z/
-
-    errors.add(:email, 'must be a valid address.')
-  end
-
-  def password_input
-    return if password =~ /\A(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{6,70}\z/
-
-    errors.add(:password, 'must be at least 6 characters long, and include 1 uppercase, 1 lowercase, 1 digit and 1 special character.')
-  end
-
-  def phone_input
-    return if phone =~ /\A[+]?[1]?[ ]?([\(]?([0-9]{3})[\)]?[ \.\-]?([0-9]{3})[ \-\.]?([0-9]{4})([ ]?((;|x|#|extension|ext)?)[ ]?([0-9]{0,6})))\z/
-
-    errors.add(:phone, 'needs to be a valid number.')
-  end
+  
+  validates_format_of :email,
+    with: /\A(([a-z0-9][\w\.\-]{0,62}[a-z0-9])@(([a-z0-9][a-z0-9\-]{0,62}[a-z0-9]\.){1,8})([a-z]{2,63}))\z/, 
+    message: 'must be a valid address.'
+  validates_format_of :password,
+    with: /\A(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{6,70}\z/, 
+    message: 'must include 1 uppercase, 1 lowercase, 1 digit and 1 special character.'
+  validates_format_of :phone,
+    with: /\A[+]?[1]?[ ]?([\(]?([0-9]{3})[\)]?[ \.\-]?([0-9]{3})[ \-\.]?([0-9]{4})([ ]?((;|x|#|extension|ext)?)[ ]?([0-9]{0,6})))\z/i, 
+    message: 'needs to be a valid number.'
 
   # Override Devise::Models::Authenticatable methods
   def active_for_authentication? 
