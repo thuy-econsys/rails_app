f0afce8d431f02b971ba76041be43bbaa57074f4
Thuy Ngo <thuy.ngo@econsys.com>
2020-11-06 01:10:31 -0500

    Add phone field to devise.registrations views for edit & new

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
+  protected
+
+  def configure_permitted_parameters 
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:phone])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:phone])
+  end
 end
