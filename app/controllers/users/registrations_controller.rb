38a53ed2a147fc72231fb092cdba1091bdf05678
Thuy Ngo <thuy.ngo@econsys.com>
2021-02-02 20:30:57 -0500

    Change page redirect after successful signup for unapproved user to correct issue with alert not displaying

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -40,13 +40,15 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
+  protected
+
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
   #   super(resource)
   # end
 
   # The path used after sign up for inactive accounts.
-  # def after_inactive_sign_up_path_for(resource)
-  #   super(resource)
-  # end
+  def after_inactive_sign_up_path_for(resource)
+    new_user_session_path
+  end
 end
