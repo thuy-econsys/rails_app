a96c8bdf25d5289a805da1dee205ac52e3926ab6
Thuy Ngo <thuy.ngo@econsys.com>
2021-02-03 20:55:09 -0500

    Add routes for dashboard; adjust specs and all green

--- spec/features/signup.rb
+++ spec/features/signup.rb
@@ -5,21 +5,18 @@ feature 'Sign up' do
 
   scenario 'is able to visit new_user_registration_path' do
     visit new_user_registration_path
-    expect(current_path).to eq('/users/sign_up')
+    expect(current_path).to eq('/account/sign_up')
     expect(page).to have_css('h1', :text => 'Sign up')    
   end
 
   context 'when sign up is successful' do
     scenario 'redirects to sign-in page' do
       sign_up(test_user)
-      expect(current_path).to eq('/users/sign_in')
+      expect(current_path).to eq('/account/sign_in')
     end
     scenario 'has successful sign-up notice' do
       sign_up(test_user)
-      expect(page).to have_css('p.alert', :text => 'You need to sign in or sign up before continuing.')
-      # FIXME should be:
-      # expect(page).to have_content("You have signed up successfully but your account has not been approved by your administrator yet.")
-      # expect(page).to have_css('p.notice', :text => 'You have signed up successfully but your account has not been approved by your administrator yet.')
+      expect(page).to have_css('p.notice', :text => 'You have signed up successfully but your account has not been approved by your administrator yet.')
     end
     scenario "has correct content" do
       sign_up(test_user)
@@ -31,7 +28,7 @@ feature 'Sign up' do
     scenario 'redirects to users path sign up page' do
       test_user[:email] =""
       sign_up(test_user)
-      expect(current_path).to eq('/users')
+      expect(current_path).to eq('/account')
       expect(page).to have_css('h1', :text => 'Sign up')
     end
     scenario 'returns error alert' do
@@ -45,7 +42,7 @@ feature 'Sign up' do
     scenario 'redirects to users path sign up page' do
       test_user[:password_confirmation] = "wrong-password"
       sign_up(test_user)
-      expect(current_path).to eq('/users')
+      expect(current_path).to eq('/account')
       expect(page).to have_css('h1', :text => 'Sign up')
     end
     scenario 'returns error alert' do
