fb740741bb0fe591dafa066a8c17707df3164d55
Thuy Ngo <thuy.ngo@econsys.com>
2021-02-04 12:50:07 -0500

    Seed User data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,34 @@
-User.create!(
-  :email => "approved@person.com",
-  :password => "password",
-  :approved => true
-)
+users = [
+  { email: 'admin@email.com',
+    password: 'P@ssw0rd',
+    approved: true,
+    phone: Faker::PhoneNumber.phone_number,
+    notes: Faker::Hacker.say_something_smart
+  },
+  {
+    email: Faker::Internet.unique.email,
+    password: 'P@ssw0rd',
+    approved: false,
+    phone: Faker::PhoneNumber.phone_number,
+    notes: Faker::Hacker.say_something_smart
+  },
+  {
+    email: Faker::Internet.unique.email,
+    password: 'P@ssw0rd',
+    approved: true,
+    phone: Faker::PhoneNumber.phone_number,
+    notes: Faker::Hacker.say_something_smart
+  }
+]
+
+users.each do |user_data|
+  user = User.find_by(email: user_data[:email])
+  user ||= User.new
+
+  user_data.each do |k,v|
+    user.send("#{k}=",v)
+  end
+  
+  user.save!(validate: false)
+  user_data[:id] = user.id
+end
