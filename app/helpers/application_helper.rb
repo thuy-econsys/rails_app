a3375328ea7d759bee2d5b54505380f47734dd89
Thuy Ngo <thuy.ngo@econsys.com>
2021-06-25 11:44:10 -0400

    Add devise mailers for confirmation and devise mailer changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+
+  STYLES = {
+    h1: 'margin-bottom: 20px; margin-top: 10px; font-size: 32px;',
+    h2: 'margin-bottom: 10px; margin-top: 15px; font-size: 18px;',
+    h3: 'margin-bottom: 10px; margin-top: 15px; font-size: 14px;',
+    a: 'text-decoration: none; color: #32aeee;',
+    p: 'margin-bottom: 10px; line-height: 20px;'    
+  }
+
+  def email_tag(type, options = {}, &content)
+    options[:style] ||= STYLES[type]
+    options[:target] = '_blank' if type == :a
+    content_tag type, options, &content
+  end
+
 end
