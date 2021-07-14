25d1e263b79a56a2846b3ffe8540a8163fe4d71c
Thuy Ngo <thuy.ngo@econsys.com>
2021-07-14 17:55:04 -0400

    Add OmniAuth atributes

--- db/migrate/20210708173923_add_omniauth_to_users.rb
+++ db/migrate/20210708173923_add_omniauth_to_users.rb
@@ -2,8 +2,5 @@ class AddOmniauthToUsers < ActiveRecord::Migration[5.2]
   def change
     add_column :users, :provider, :string
     add_column :users, :uid, :string
-    add_column :users, :avatar_url, :string
-    add_column :users, :full_name, :string
-    add_index :users, :uid
   end
 end
