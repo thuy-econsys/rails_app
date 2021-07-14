8dca7537ac3517d9f7873ee46c5ad1618a8a793d
Thuy Ngo <thuy.ngo@econsys.com>
2021-07-14 15:06:59 -0400

    Add OmniAuthable Devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users, path: "account", :controllers => { registrations: 'users/registrations' }
+  devise_for :users, 
+  path: 'account',
+  controllers: { 
+    registrations: 'users/registrations',
+    omniauth_callbacks: 'users/omniauth_callbacks'
+  }
 
   get     '/users',           to: 'pages#index',    as: :dashboard
   get     '/users/:id/edit',  to: 'pages#edit',     as: :edit_user
